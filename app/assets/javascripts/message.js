0333339aeb8c69239abb5b180c8f439fb4726db9
teri0328 <jack.teri@icloud.com>
2020-03-17 19:45:30 +0900

    自動更新機能

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,24 +1,26 @@
 $(function(){ 
   var buildHTML = function(message) {
     if (message.content && message.image) {
+      //data-idが反映されるようにしている
       var html = 
-      `<div class="message" data-message-id=${message.id}>
-        <div class="upper-message">
-          <div class="upper-message__user-name">
-            ${message.user_name}
+        `<div class="message" data-message-id=${message.id}>
+          <div class="upper-message">
+            <div class="upper-message__user-name">
+              ${message.user_name}
+            </div>
+            <div class="upper-message__date">
+              ${message.created_at}
+            </div>
           </div>
-          <div class="upper-message__date">
-            ${message.created_at}
+          <div class="lower-message">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+            <img src=${message.image} class=${lower-message__image} >
           </div>
-        </div>
-        <div class="lower-message">
-          <p class="lower-message__content">
-            ${message.content}
-          </p>
-        </div>
-        <img src=${message.image} >
-      </div>`
+        </div>`
     } else if (message.content) {
+      //同様に、data-idが反映されるようにしている
       var html = 
         `<div class="message" data-message-id=${message.id}>
           <div class="upper-message">
@@ -36,8 +38,9 @@ $(function(){
           </div>
         </div>`
     } else if (message.image) {
+      //同様に、data-idが反映されるようにしている
       var html = 
-        `<div class="message" data-message-id=${message.ida}>
+        `<div class="message" data-message-id=${message.id}>
           <div class="upper-message">
             <div class="upper-message__user-name">
               ${message.user_name}
@@ -47,7 +50,7 @@ $(function(){
             </div>
           </div>
           <div class="lower-message">
-            <img src=${message.image} >
+            <img src=${message.image} class=${lower-message__image}>
           </div>
         </div>`
     };
@@ -63,10 +66,13 @@ $(function(){
     })
     .done(function(messages) {
       if (messages.length !== 0) {
+        //追加するHTMLの入れ物を作る
         var insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
         $.each(messages, function(i, message) {
           insertHTML += buildHTML(message)
         });
+        //メッセージが入ったHTMLに、入れ物ごと追加
         $('.messages').append(insertHTML);
         $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
       }
@@ -75,5 +81,7 @@ $(function(){
       alert('error');
     });
   };
-  setInterval(reloadMessages, 7000);
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
